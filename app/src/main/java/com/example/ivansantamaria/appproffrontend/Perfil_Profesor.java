1857628abf70b21c381b197fa3bcba5ca31d3e66
isak <isakedo@gmail.com>
2017-03-31 17:28:04 +0200

    Perfil Prof

---
+++ app/src/main/java/com/example/ivansantamaria/appproffrontend/Perfil_Profesor.java
@@ -0,0 +1,13 @@
+package com.example.ivansantamaria.appproffrontend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Perfil_Profesor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil__profesor);
+    }
+}
