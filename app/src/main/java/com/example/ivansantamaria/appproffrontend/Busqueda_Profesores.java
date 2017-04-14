7d839caef80839ea26248291cac0888d21ce8b2d
fiusa8 <dfusteropalacios@gmail.com>
2017-04-14 13:06:41 +0200

    Transiciones de busqueda y listar

--- app/src/main/java/com/example/ivansantamaria/appproffrontend/Busqueda_Profesores.java
+++ app/src/main/java/com/example/ivansantamaria/appproffrontend/Busqueda_Profesores.java
@@ -1,13 +1,138 @@
 package com.example.ivansantamaria.appproffrontend;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class Busqueda_Profesores extends AppCompatActivity {
+
+    //private String nombreUsuario;
+
+    private EditText nombre;
+    private EditText ciudad;
+    private Spinner horarios;
+    private Spinner asignaturas;
+    private Spinner cursos;
+
+    private Button buscarProfesor = null;
+    private Button favoritoProfesor = null;
+
+    /** identificador para la actividad de listar profesores de busqueda */
+    private static final int ACTIVITY_LISTAR_BUSQUEDA=0;
+    /** identificador para la actividad de listar profesores favoritos */
+    private static final int ACTIVITY_LISTAR_FAVORITOS=1;
+
+    private static final ArrayList<String> horariosList = new ArrayList<String>()
+    {{ add("---"); add("Mañanas"); add("Tardes");}};
+
+    private static final ArrayList<String> asignaturasList = new ArrayList<String>()
+    {{ add("---"); add("Matematicas"); add("Fisica"); add("Ingles"); add("Frances"); add("Biologia");}};
+
+    private static final ArrayList<String> cursosList = new ArrayList<String>()
+    {{ add("---"); add("Primaria"); add("Secundaria"); add("Bachillerato"); add("Universidad");}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busqueda__profesores);
+
+        final com.example.ivansantamaria.appproffrontend.Busqueda_Profesores local = this;
+        buscarProfesor = (Button) findViewById(R.id.buscarProfesor);
+        favoritoProfesor = (Button) findViewById(R.id.favoritoProfesor);
+
+        // Bundle extras = getIntent().getExtras();
+        //Recoger informacion del usuario de la sesion
+        //nombreUsuario = (extras != null) ? extras.getString("nombreUsuario") : null;
+
+        populatefields();
+
+        buscarProfesor.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                if(nombre.getText().toString().equals("") &&
+                        ciudad.getText().toString().equals("") &&
+                        horarios.getSelectedItemPosition() == 0 &&
+                        asignaturas.getSelectedItemPosition() == 0 &&
+                        cursos.getSelectedItemPosition() == 0)
+                {
+                    AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(local);
+
+                    dlgAlert.setMessage("Es necesario rellenar al menos un campo");
+                    dlgAlert.setTitle("Error...");
+                    dlgAlert.setPositiveButton("OK", null);
+                    dlgAlert.setCancelable(true);
+                    dlgAlert.create().show();
+
+                    dlgAlert.setPositiveButton("Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                } else {
+                    //PEDIR A LA BASE DE DATOS QUE REALICE LA BUSQUEDA DEL PROFESOR
+                    listar_busqueda();
+                }
+            }
+        });
+
+        favoritoProfesor.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                listar_favoritos();
+            }
+        });
+    }
+
+    private void populatefields(){
+        nombre = (EditText) findViewById(R.id.usuarioProfesorBusqueda);
+        ciudad = (EditText) findViewById(R.id.ciudadProfesorBusqueda);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, R.layout.row_spinner, horariosList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        horarios = (Spinner) findViewById(R.id.horariosProfesorBusqueda);
+        horarios.setAdapter(adapter);
+
+        adapter = new ArrayAdapter<>(
+                this, R.layout.row_spinner, asignaturasList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        asignaturas = (Spinner) findViewById(R.id.asignaturasProfesorBusqueda);
+        asignaturas.setAdapter(adapter);
+
+        adapter = new ArrayAdapter<>(
+                this, R.layout.row_spinner, cursosList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cursos = (Spinner) findViewById(R.id.cursoProfesorBusqueda);
+        cursos.setAdapter(adapter);
+    }
+
+    private void listar_favoritos(){
+        Intent i = new Intent(this, Listar_Profesores.class); //Profesores_Favoritos.class);
+        //i.putExtra("nombreUsuario", nombreUsuario);
+        startActivityForResult(i, ACTIVITY_LISTAR_FAVORITOS);
+    }
+
+    private void listar_busqueda(){
+        Intent i = new Intent(this, Listar_Profesores.class);
+        //i.putExtra("nombreUsuario", nombreUsuario);
+        i.putExtra("nombre", nombre.getText().toString());
+        i.putExtra("ciudad", ciudad.getText().toString());
+        String horario = horariosList.get(horarios.getSelectedItemPosition());
+        i.putExtra("horario", horario);
+        String asignatura = asignaturasList.get(asignaturas.getSelectedItemPosition());
+        i.putExtra("asignatura", asignatura);
+        String curso = cursosList.get(cursos.getSelectedItemPosition());
+        i.putExtra("curso", curso);
+        startActivityForResult(i, ACTIVITY_LISTAR_BUSQUEDA);
     }
 }
--- app/src/main/java/com/example/ivansantamaria/appproffrontend/Listar_Profesores.java
+++ app/src/main/java/com/example/ivansantamaria/appproffrontend/Listar_Profesores.java
@@ -1,13 +1,117 @@
 package com.example.ivansantamaria.appproffrontend;
 
+import android.content.Intent;
+import android.database.MatrixCursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Listar_Profesores extends AppCompatActivity {
+
+    private String profesor;
+
+    private String nombre;
+    private String ciudad;
+    private String horario;
+    private String asignatura;
+    private String curso;
+
+    private ListView listView;
+    private ArrayList<ProfesorVO> m_profesores;
+
+    /** identificador para la actividad de ver profesor seleccionado */
+    private static final int ACTIVITY_VER_PROFESOR=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar__profesores);
+
+        listView = (ListView) findViewById(R.id.list);
+
+        // Recogemos los datos para realizar la busqueda
+        Bundle extras = getIntent().getExtras();
+
+
+        // Nombre y ciudad si no han sido rellenados -> ""
+        nombre = (extras != null) ? extras.getString("nombre") : null;
+        ciudad = (extras != null) ? extras.getString("ciudad") : null;
+        // Horario, asignatura y curso si no han sido rellenados -> "---"
+        horario = (extras != null) ? extras.getString("horario") : null;
+        asignatura = (extras != null) ? extras.getString("asignatura") : null;
+        curso = (extras != null) ? extras.getString("curso") : null;
+
+        fillData();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView c = (TextView) view.findViewById(R.id.nombreProfesorListar);
+                profesor = c.getText().toString();
+                verProfesor();
+            }
+        });
+
     }
+
+    /**
+     * Rellena la lista de notas con la información de la base de datos.
+     */
+    private void fillData() {
+        // Acceso a la base de datos
+        Facade facade = new Facade();
+        //Buscamos los profesores
+        m_profesores = new ArrayList<>();//Facade.buscarProfesores(nombre,ciudad, horario, asignatura, curso);
+        m_profesores.add(facade.perfilProfesor("David"));
+        m_profesores.add(facade.perfilProfesor("Fuste"));
+        // Si no existen profesores se muestra mensaje
+        if (m_profesores.isEmpty()){
+            TextView empty = (TextView)findViewById(R.id.empty);
+            empty.setVisibility(View.VISIBLE);
+        }
+        // Si si que existen se procede a mostrarlos
+        else {
+
+            // Create an array to specify the fields we want to display in the list
+            String[] from = new String[]{"Nombre", "Ciudad",
+                    "Asignaturas", "Horarios", "Valoración media"};
+
+            // Se crea un cursor para mostrar los profesores
+            MatrixCursor profesorCursor = new MatrixCursor(
+                    new String[]{"_id", "Nombre", "Ciudad", "Asignaturas",
+                            "Horarios", "Valoración media"});
+            startManagingCursor(profesorCursor);
+
+            // Se añaden los profesores encontrados al cursor
+            for (int i = 0; i < m_profesores.size(); i++) {
+                ProfesorVO p = m_profesores.get(i);
+                profesorCursor.addRow(new Object[]{i, p.getNombreUsuario(), p.getCiudad(),
+                        p.getAsignaturas(), p.getHorarios(), p.getValoracion()});
+            }
+
+            // and an array of the fields we want to bind those fields to
+            int[] to = new int[]{R.id.nombreProfesorListar, R.id.ciudadProfesorListar,
+                    R.id.asignaturasProfesorListar, R.id.horariosProfesorListar,
+                    R.id.valoracionesProfesorListar};
+
+            // Now create an array adapter and set it to display using our row
+            SimpleCursorAdapter profesor =
+                    new SimpleCursorAdapter(this, R.layout.row_listar_profesores, profesorCursor,
+                            from, to);
+            listView.setAdapter(profesor);
+        }
+    }
+
+    private void verProfesor(){
+        Intent i = new Intent(this, Ver_Profesor.class);
+        i.putExtra("nombreUsuario", profesor);
+        startActivityForResult(i, ACTIVITY_VER_PROFESOR);
+    }
+
 }
